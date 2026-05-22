04b476f57a3dfffccc0c96cc3dac1dda0ee1d2de
kiskm <keisuke.matsuda0213@gmail.com>
2026-05-22 00:00:17 -0700

    feat: セキュリティグループの設定
    AWSのセキュリティグループの構成・ルールをTerraformで追加しました。
    インバウンド:
    - 3000番(Front)
    - 8000番(API)
    - 22番(SSH)
    アウトバウンド:
    すべてのトラフィック

---
+++ security-group.tf
@@ -0,0 +1,61 @@
+# ------------------------------
+# My IP
+# ------------------------------
+data "http" "my_ip" {
+  url = "https://api.ipify.org?format=json"
+}
+
+locals {
+  my_ip = "${jsondecode(data.http.my_ip.response_body)["ip"]}/32"
+}
+
+# ------------------------------
+# Security Group
+# ------------------------------
+resource "aws_security_group" "web-sg" {
+  name        = "${var.project}-${var.environment}-web-sg"
+  description = "web server security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-web-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_security_group_rule" "front_in_tcp3000" {
+  security_group_id = aws_security_group.web-sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 3000
+  to_port           = 3000
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "api_in_tcp8000" {
+  security_group_id = aws_security_group.web-sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 8000
+  to_port           = 8000
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "opmng_in_ssh" {
+  security_group_id = aws_security_group.web-sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_blocks       = [local.my_ip]
+}
+
+resource "aws_security_group_rule" "all_out" {
+  security_group_id = aws_security_group.web-sg.id
+  type              = "egress"
+  protocol          = -1
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+}
